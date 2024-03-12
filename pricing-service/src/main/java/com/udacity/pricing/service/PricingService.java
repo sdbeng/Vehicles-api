c640cd973353e363b95924187b541ff7b877a356
Sergio D. Barrera <sdbeng@gmail.com>
2024-03-11 19:04:21 -0700

    Add test to check if app generates a price for a given vechicle id

--- pricing-service/src/main/java/com/udacity/pricing/service/PricingService.java
+++ pricing-service/src/main/java/com/udacity/pricing/service/PricingService.java
@@ -24,7 +24,7 @@ public class PricingService {
 
     /**
      * If a valid vehicle ID, gets the price of the vehicle from the stored array.
-     * @param vehicleId ID number of the vehicle the price is requested for.
+     *
      * @return price of the requested vehicle
      * @throws PriceException vehicleID was not found
      */
